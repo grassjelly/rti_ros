d30b86d84ae92a5a4e227ca867df550fba78c1b6
Juan Jimeno <jimenojmm@gmail.com>
2017-06-12 17:30:30 +0800

    fixed CMake and generator that prevents from having more than one package in a workspace

--- gencodes.sh
+++ gencodes.sh
@@ -19,6 +19,10 @@ cd ..
 #change the filenames on CMakeLists based on newly generated codes
 perl -pi -e "s/$PREV_IDL/$CURRENT_IDL/g" CMakeLists.txt
 
+#change library name based on idl
+perl -pi -e "s/$PREV_IDL_lib/$CURRENT_IDL_lib/g" CMakeLists.txt
+
+
 #store the previous idl name so the bash file knows what to replace next call
 perl -pi -e "s/$PREV_IDL/$CURRENT_IDL/g" gencodes.cache
 
